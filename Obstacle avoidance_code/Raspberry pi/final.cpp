9f680f552a3bf6f38792107d15f84d366ec9fe2c
pai666858 <3010324M@student.gla.ac.uk>
2025-06-19 14:39:49 +0100

    Update final.cpp

--- Obstacle avoidance_code/Raspberry pi/final.cpp
+++ Obstacle avoidance_code/Raspberry pi/final.cpp
@@ -146,7 +146,7 @@ int main() {
 
     this_thread::sleep_for(chrono::seconds(2));
     sendCommand('p');
-    cout << "🚗 Default to automatic mode (p)，Switchable m=manual f=follow ESC=exit\n";
+    cout << "🚗 Default to automatic mode (p)，can switch m=manual f=follow ESC=exit\n";
 
     thread reader(readArduino);
     thread camera(followAruco);
