5eb391897023539827479b4cfd98ce22966b4506
pai666858 <3010324M@student.gla.ac.uk>
2025-06-23 11:12:13 +0100

    Update robot.cpp

--- Obstacle avoidance_version1/robot.cpp
+++ Obstacle avoidance_version1/robot.cpp
@@ -3,14 +3,14 @@
 #include <chrono>
 
 Robot::Robot() {
-    motors[0] = new Motor(0, 1, 26);   // 左前
-    motors[1] = new Motor(2, 3, 23);   // 左后
-    motors[2] = new Motor(21, 22, 28); // 右前
-    motors[3] = new Motor(24, 25, 29); // 右后
+    motors[0] = new Motor(0, 1, 26);   // Front left
+    motors[1] = new Motor(2, 3, 23);   // Rear left
+    motors[2] = new Motor(21, 22, 28); // Front right
+    motors[3] = new Motor(24, 25, 29); // Rear right
 
-    sensors[0] = new Ultrasonic(15, 16, 0); // 前
-    sensors[1] = new Ultrasonic(4, 5, 1);   // 左
-    sensors[2] = new Ultrasonic(6, 7, 2);   // 右
+    sensors[0] = new Ultrasonic(15, 16, 0); // Front
+    sensors[1] = new Ultrasonic(4, 5, 1);   // Left
+    sensors[2] = new Ultrasonic(6, 7, 2);   // Right
 
     for (int i = 0; i < 3; ++i) {
         sensors[i]->registerCallback(this);
@@ -49,7 +49,7 @@ void Robot::onTooClose(float distance, int sensorId) {
     stopAll();
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
-    if (sensorId == 0) { // 前方
+    if (sensorId == 0) { // Front
         float leftDist = sensors[1]->getDistance();
         float rightDist = sensors[2]->getDistance();
         if (leftDist > rightDist) turnLeft();
