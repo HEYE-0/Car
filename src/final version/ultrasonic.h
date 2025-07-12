13010b10dbb95eac0b32549c4068ee029d7aabb1
Ye He <2966900H@student.gla.ac.uk>
2025-07-12 11:18:24 +0100

    Update ultrasonic.h

--- src/final version/ultrasonic.h
+++ src/final version/ultrasonic.h
@@ -3,30 +3,53 @@
 
 #include <gpiod.h>
 #include <thread>
-#include <string>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
 
 class DistanceEventInterface {
 public:
-    virtual void onTooClose(float distance, int sensorId) = 0;
+    virtual void onTooClose(float distance, int id) = 0;
 };
 
 class Ultrasonic {
 public:
     Ultrasonic(const std::string& chipname, int trig_line, int echo_line, int id);
     ~Ultrasonic();
-    float getDistance();
+
     void registerCallback(DistanceEventInterface* cb);
     void start();
+    void notifyMeasure();
+    void stop();
 
 private:
-    std::string _chipname;
-    int _trigLine, _echoLine, _id;
-    gpiod_chip* _chip;
-    gpiod_line* _trig;
-    gpiod_line* _echo;
-    DistanceEventInterface* _callback;
     void monitorLoop();
+
+    enum class State {
+        IDLE,
+        TRIGGER,
+        WAIT_FOR_RISING,
+        WAIT_FOR_FALLING
+    };
+
+    struct gpiod_chip* _chip;
+    struct gpiod_line* _trig;
+    struct gpiod_line* _echo;
+
+    std::string _chipname;
+    int _trigLine;
+    int _echoLine;
+    int _id;
+
+    DistanceEventInterface* _callback;
     std::thread _thread;
+
+    std::mutex _mutex;
+    std::condition_variable _cv;
+    bool _running;
+    bool _shouldMeasure;
+    State _state;
+    std::chrono::steady_clock::time_point _echoStart;
 };
 
 #endif
