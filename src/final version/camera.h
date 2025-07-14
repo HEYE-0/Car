0451c00b29603de1f46c8522c1690c44fa812254
Ye He <2966900H@student.gla.ac.uk>
2025-07-14 12:05:52 +0100

    Update camera.h

--- src/final version/camera.h
+++ src/final version/camera.h
@@ -1,23 +1,30 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
 #include <thread>
 
 class CameraEventInterface {
 public:
     virtual void onMarkerDetected(int id, cv::Point2f pos) = 0;
+    virtual ~CameraEventInterface() = default;
 };
+
 
 class Camera {
 public:
+ 
     Camera();
+
     ~Camera();
 
     void registerCallback(CameraEventInterface* cb);
 
 private:
+
     void processLoop();
+
     bool detectMarker(cv::Mat &frame, int &id, cv::Point2f &pos);
 
     cv::VideoCapture cap;
@@ -26,4 +33,4 @@ private:
     bool running;
 };
 
-#endif
+#endif // CAMERA_H
